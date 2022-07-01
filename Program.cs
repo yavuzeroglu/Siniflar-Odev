336065818eb3060ece27bad93b1f061d0673500b
Yavuz Eroğlu <106495712+yavuzeroglu@users.noreply.github.com>
2022-07-02 01:44:21 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sinif_Odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitaplar kt = new Kitaplar();
+            
+            Console.WriteLine
+                ("Kitap Adını Giriniz :");
+            kt.kitapAd = Console.ReadLine();
+            
+            Console.WriteLine
+                ("Yazarın Adını Giriniz :");
+            kt.Yazar = Console.ReadLine();
+            
+            Console.WriteLine
+                ("Kitabın Sayfa Sayısını Giriniz :");
+            kt.sayfaSayisi = Convert.ToInt32(Console.ReadLine());    
+            kt.KitapYazdir();
+        }
+    }
+    class Kitaplar{
+        public string kitapAd;
+        public string Yazar;
+        public int sayfaSayisi;
+
+        public void KitapYazdir(){
+            Console.WriteLine("************");
+            Console.WriteLine("Kitabın Adı :{0}",kitapAd);
+            Console.WriteLine("Kitabın Yazarı :{0}",Yazar);
+            Console.WriteLine("Kitabın Sayfa Sayisi :{0}",sayfaSayisi);
+        }
+
+    }
+}
